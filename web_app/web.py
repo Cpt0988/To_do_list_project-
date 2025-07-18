6d3e0c7b6699e29aff7a041dd051792c90e6c30a
Chuck Taylor <cpt2948@gmail.com>
2025-07-17 20:52:52 -0400

    Put files in web app folder

---
+++ web_app/web.py
@@ -0,0 +1,32 @@
+#lib to create web app & function
+import streamlit as st
+import function
+
+todos= function.get_todos()
+
+def add_todo():
+    todo=st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    function.write_todos(todos)
+
+todos = function.get_todos()
+
+st.title("My Todo App")
+st.subheader("My todo list")
+st.write("This app to have you be more productive.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+    
+st.text_input(label="", 
+              placeholder= "Add todo..", 
+              on_change= add_todo,
+              key='new_todo')
+
+
+#st.button(label="Submit",)
